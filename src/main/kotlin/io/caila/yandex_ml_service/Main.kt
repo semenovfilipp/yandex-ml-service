331372d989ca347b8b3b2175cbfa69ebd7dc4e83
semenovfilipp <znexiss@gmail.com>
2024-02-16 21:01:44 +0300

    feat : application first start

--- src/main/kotlin/io/caila/yandex_ml_service/Main.kt
+++ src/main/kotlin/io/caila/yandex_ml_service/Main.kt
@@ -1,15 +1,21 @@
 package io.caila.yandex_ml_service
 
-import com.mlp.sdk.MlpClientSDK
-import com.mlp.sdk.MlpService
+import com.mlp.sdk.MlpExecutionContext
+import com.mlp.sdk.MlpPredictServiceBase
 import com.mlp.sdk.MlpServiceSDK
 import com.mlp.sdk.datatypes.chatgpt.ChatCompletionRequest
 import com.mlp.sdk.datatypes.chatgpt.ChatCompletionResult
 
-class Main :MlpSer {
+class Main(
+    override val context: MlpExecutionContext
+) : MlpPredictServiceBase<ChatCompletionRequest, ChatCompletionResult>(REQUEST_EXAMPLE, RESPONSE_EXAMPLE) {
+
+    private val yandexChatService = YandexChatService()
+
     override fun predict(req: ChatCompletionRequest): ChatCompletionResult {
-        val message = sendMessageToYandex(req)
-        val result = ChatCompletionResult(
+        val message = yandexChatService.sendMessageToYandex(req)
+
+        return ChatCompletionResult(
             null,
             message,
             created = System.currentTimeMillis(),
@@ -17,11 +23,37 @@ class Main :MlpSer {
             emptyList(),
             null
         )
-        return result
+    }
+
+    companion object {
+        val REQUEST_EXAMPLE = ChatCompletionRequest(
+            model = null,
+            messages = emptyList(),
+            temperature = null,
+            topP = null,
+            n = null,
+            stream = null,
+            stop = emptyList(),
+            maxTokens = null,
+            presencePenalty = null,
+            frequencyPenalty = null,
+            logitBias = null,
+            user = null
+        )
+        val RESPONSE_EXAMPLE = ChatCompletionResult(
+            null,
+            "message",
+            created = System.currentTimeMillis(),
+            "",
+            emptyList(),
+            null
+        )
     }
 }
+
 fun main() {
-    val service = MlpServiceSDK
+    val actionSDK = MlpServiceSDK({ Main(MlpExecutionContext.systemContext) })
+
     actionSDK.start()
     actionSDK.blockUntilShutdown()
 }
